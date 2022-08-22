3fdbf4c7e33672d35f1eebea074e0bb7ff4cedd9
Pedro Batista <67802288+pedromachakio@users.noreply.github.com>
2022-08-22 13:44:02 +0000

    created current user and register model

--- src/app/auth/state/actions.ts
+++ src/app/auth/state/actions.ts
@@ -1,7 +1,8 @@
 import { createAction, props } from '@ngrx/store';
+import { RegisterRequestInterface } from '../models/registerRequest.interface';
 import { ActionTypes } from './actionTypes';
 
 export const registerAction = createAction(
   ActionTypes.Register,
-  props<{ username: string; password: string; email: string }>()
+  props<{ request: RegisterRequestInterface }>()
 );
